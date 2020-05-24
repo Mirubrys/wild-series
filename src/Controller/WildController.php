1a585622893728a563b78cd2cfea3269a485f244
Mirubrys <mic.mendesdos@gmail.com>
2020-05-24 12:32:09 +0200

    Modified the method 'index' to display a search form for programs and added some comments.

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,7 +7,9 @@ use App\Entity\Category;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,22 +30,48 @@ class WildController extends AbstractController
      *     "/index",
      *     name="index"
      * )
+     * @param Request $request
      * @return Response
      */
-    public function index() :Response
+    public function index(Request $request) :Response
     {
-        $programs = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findAll();
-        if (!$programs) {
-            throw $this->createNotFoundException(
-                'No program found in program\'s table.'
-            );
+        /* We instantiate a search form */
+        $form = $this->createForm(ProgramSearchType::class);
+        $form->handleRequest($request);
+
+        /* If the user make a search, we get the matching ones,
+        Else, we get all programs */
+        if ($form->isSubmitted()) {
+            // We get the _POST
+            $search = $form->getData()['searchField'];
+
+            /* We query the table 'program' in db */
+            $programs = $this->getDoctrine()
+                ->getRepository(Program::class)
+                ->createQueryBuilder('p')
+                ->where('p.title LIKE :search')
+                ->setParameter('search', '%'.$search.'%')
+                ->getQuery()
+                ->getResult();
+        } else {
+            /* We get all programs */
+            $programs = $this->getDoctrine()
+                ->getRepository(Program::class)
+                ->findAll();
+
+            /* If no program where found, we throw a http code 404 */
+            if (!$programs) {
+                throw $this->createNotFoundException(
+                    'No program found in program\'s table.'
+                );
+            }
         }
 
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
             'programs' => $programs,
+            'form' => $form->createView(),
+            'search' => $form->isSubmitted()
         ]);
     }
 
@@ -60,20 +88,27 @@ class WildController extends AbstractController
      */
     public function show(?string $slug):Response
     {
+        /* If no slug is given, we throw a http code 404 */
         if (!$slug) {
             throw $this
                 ->createNotFoundException(
                     'No slug has been sent to find a program in program\'s table.'
                 );
         }
+
+        // We format the slug
         $slug = preg_replace(
             '/-/',
             ' ', ucwords(trim(strip_tags($slug)), "-")
         );
+
+        /* We get the program by its title */
         $program = $this
             ->getDoctrine()
             ->getRepository(Program::class)
             ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        /* If there is no program returned, we throw a http code 404 */
         if (!$program) {
             throw $this
                 ->createNotFoundException(
@@ -81,6 +116,7 @@ class WildController extends AbstractController
             );
         }
 
+        // We get all seasons of the got program
         $seasons = $program->getSeasons();
 
         return $this->render('wild/show.html.twig', [
@@ -103,14 +139,18 @@ class WildController extends AbstractController
      */
     public function showByCategory(?string $categoryName) :Response
     {
+        /* If there is not category given, we throw a http code 404 */
         if (!$categoryName) {
             throw $this
                 ->createNotFoundException(
                     'No category has been sent.'
                 );
         }
+
+        // We format the category name
         $categoryName = ucwords(trim(strip_tags($categoryName)),"-");
 
+        /* We get the category by its name */
         $category = $this
             ->getDoctrine()
             ->getRepository(Category::class)
@@ -118,6 +158,7 @@ class WildController extends AbstractController
                 "name" => $categoryName
             ]);
 
+        /* If there is no category returned, we throw a http code 404 */
         if (!$category) {
             throw $this
                 ->createNotFoundException(
@@ -125,6 +166,7 @@ class WildController extends AbstractController
                 );
         }
 
+        /* We get the 3 last programs of the category */
         $programs = $this
             ->getDoctrine()
             ->getRepository(Program::class)
@@ -158,24 +200,32 @@ class WildController extends AbstractController
      */
     public function showBySeason(int $id) :Response
     {
+        /* If there is no id given, we throw a http code 404 */
         if(!$id) {
             throw $this
                 ->createNotFoundException(
                     'No season has been send.'
                 );
         }
+
+        /* We get the season by it's id */
         $season = $this
             ->getDoctrine()
             ->getRepository(Season::class)
             ->find($id);
 
+        /* If there is no season returned, we throw a http code 404 */
         if (!$season) {
             throw $this
                 ->createNotFoundException(
                     'Doesn\'t exists'
                 );
         }
+
+        // We get the program relative to the season
         $program = $season->getProgram();
+
+        // We get the episodes of the season
         $episodes = $season->getEpisodes();
 
         return $this->render('wild/season.html.twig', [
@@ -198,7 +248,10 @@ class WildController extends AbstractController
      */
     public function showEpisode(Episode $episode) :Response
     {
+        // We get the season relative to the episode
         $season = $episode->getSeason();
+
+        // We get the program relative to the episode
         $program = $season->getProgram();
 
         return $this->render('wild/episode.html.twig', [
