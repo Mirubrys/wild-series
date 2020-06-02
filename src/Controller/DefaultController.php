496ea56bc1454131114b9c429888ec33490acbf7
Mirubrys <mic.mendesdos@gmail.com>
2020-06-02 08:35:42 +0200

    Modifed style of the homepage and navbar.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +15,23 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render( 'home.html.twig');
+        /* We get the 3 last programs */
+        $programs = $this
+            ->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(
+            // Search programs by category
+                [],
+                // Ordered by id
+                ["id" => 'desc'],
+                // Limited by 3 results
+                3,
+                // Offset
+                0
+            );
+
+        return $this->render('home.html.twig', [
+            'programs' => $programs,
+        ]);
     }
 }
