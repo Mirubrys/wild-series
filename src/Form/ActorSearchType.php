64f0ecebbbe70a18de4b6c49807cd8691b0ba92b
Mirubrys <mic.mendesdos@gmail.com>
2020-06-02 21:14:40 +0200

    Created the 'Actor' entity.

---
+++ src/Form/ActorSearchType.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ActorSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('searchField', TextType::class, [
+                'label' => 'Recherche',
+                'block_name' => 'field',
+            ]);
+    }
+}
---
+++ src/Repository/ActorRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Actor;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Actor|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Actor|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Actor[]    findAll()
+ * @method Actor[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ActorRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Actor::class);
+    }
+
+    /**
+     * Return the result of a name search
+     *
+     * @param string $search
+     * @return int|mixed|string
+     */
+    public function findByNameSearch(?string $search)
+    {
+        return $this->createQueryBuilder('a')
+            ->where('a.name LIKE :search')
+            ->setParameter('search', '%'.$search.'%')
+            ->getQuery()
+            ->getResult();
+    }
+
+    // /**
+    //  * @return Actor[] Returns an array of Actor objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Actor
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
